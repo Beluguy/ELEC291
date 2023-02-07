242f8f7bd42820bc7517e329da1df4ec297a76fe
Marcus <75653877+Beluguy@users.noreply.github.com>
2023-02-07 12:31:07 -0800

    Update oven.asm

--- reflow-oven-controller/FSM_UI_UX/oven.asm
+++ reflow-oven-controller/FSM_UI_UX/oven.asm
@@ -19,6 +19,14 @@ y:   ds 4
 bcd: ds 5
 Result: ds 2
 
+;----for FSM---------------------------------------
+state: ds 1				
+soak_temp: ds 1
+soak_time: ds 1
+reflow_temp: ds 1
+reflow_time: ds 1
+;---------------------------------------------------
+
 BSEG
 mf: dbit 1
 
@@ -230,5 +238,47 @@ putchar:
     clr TI
     mov SBUF, a
     ret
+
+;-----------------------------FSM-----------------------------------
+mov a, state
+
+state0:
+	cjne a, #0, state1
+	mov pwm, #0
+	jb PB6, state0_done
+	jnb PB6, $ ; Wait for key release
+	mov state, #1
+state0_done:	
+	ljmp forever
+
+
+state1:
+	cjne a, #1, state2
+	mov pwm, #100
+	mov sec, #0
+	mov a, soak_temp
+	clr c
+	subb a, temp
+	jnc state1_done
+	mov state, #2
+state1_done:
+	ljmp forever
+
+
+state2:
+	cjne a, #2, state3
+	mov pwm, #20
+	mov a, soak_time
+	clr c
+	subb a, sec
+	jnc state2_done
+	mov state, #3
+state2_done:
+	ljmp forever
+
+
+
+
+;---------------------------------------------------------------
 
 END
