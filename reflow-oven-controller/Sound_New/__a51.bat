a41c78c44d84291d7dfbd2050f2dc86b26c65863
Marcus <75653877+Beluguy@users.noreply.github.com>
2023-02-16 12:56:31 -0800

    added new sound file

--- reflow-oven-controller/Sound_New/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\aakaa\OneDrive\Documents\0000.ELEC 2ND YR\ELEC 291\PRJ 1 GITHUB\Elec-291\reflow-oven-controller\Sound_New\"
-"C:\Users\aakaa\OneDrive\Documents\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\aakaa\OneDrive\Documents\0000.ELEC 2ND YR\ELEC 291\PRJ 1 GITHUB\Elec-291\reflow-oven-controller\Sound_New\V2_AT89LP51RC2_Receiver.asm"
-echo Crosside_Action Set_Hex_File C:\Users\aakaa\OneDrive\Documents\0000.ELEC 2ND YR\ELEC 291\PRJ 1 GITHUB\Elec-291\reflow-oven-controller\Sound_New\V2_AT89LP51RC2_Receiver.HEX
