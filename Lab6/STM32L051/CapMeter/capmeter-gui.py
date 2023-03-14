e7c7199cd057d55a4c1922eb20e6fb953e62195c
Joe <skipfie@gmail.com>
2023-03-14 14:23:03 -0700

    add third mem location and units display

--- Lab6/STM32L051/CapMeter/capmeter-gui.py
+++ Lab6/STM32L051/CapMeter/capmeter-gui.py
@@ -3,14 +3,19 @@ import tkinter as tk
 import json
 
 # Set up the serial connection
-ser = serial.Serial('COM3', 115200)  # Replace 'COM3' with the correct port for your device
+ser = serial.Serial('COM3',
+                    115200,
+                    timeout=1,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_TWO,
+                    bytesize=serial.EIGHTBITS)  # Replace 'COM3' with the correct port for your device
 ser.flushInput()
 
 # Create the GUI window
 root = tk.Tk()
 root.title("Serial Data")
 
-capacitance_heading = tk.Label(root, text="Capacitance")
+capacitance_heading = tk.Label(root, text="Capacitance [??]")
 capacitance_heading.grid(row=0,column=0)
 
 frequency_heading = tk.Label(root, text="Frequency")
@@ -23,10 +28,10 @@ capacitance_label.grid(row=1,column=0)
 frequency_label = tk.Label(root, text="No frequency detected.")
 frequency_label.grid(row=1,column=1)
 
-capmem_heading = tk.Label(root, text="Prev Capacitance")
+capmem_heading = tk.Label(root, text="Mem1 Capacitance")
 capmem_heading.grid(row=2,column=0)
 
-freqmem_heading = tk.Label(root, text="Prev Capacitance")
+freqmem_heading = tk.Label(root, text="Mem1 Frequency")
 freqmem_heading.grid(row=2,column=1)
 
 capmem_label = tk.Label(root, text="No capacitance measured yet.")
@@ -35,8 +40,17 @@ capmem_label.grid(row=3,column=0)
 freqmem_label = tk.Label(root, text="No frequency detected.")
 freqmem_label.grid(row=3,column=1)
 
-cap = 0
-freq = 0
+capmem1_heading = tk.Label(root, text="Mem2 Capacitance")
+capmem1_heading.grid(row=4,column=0)
+
+freqmem1_heading = tk.Label(root, text="Mem3 Frequency")
+freqmem1_heading.grid(row=4,column=1)
+
+capmem1_label = tk.Label(root, text="No capacitance measured yet.")
+capmem1_label.grid(row=5,column=0)
+
+freqmem1_label = tk.Label(root, text="No frequency detected.")
+freqmem1_label.grid(row=5,column=1)
 
 def read_serial():
     # Read a line of data from the serial port
@@ -44,15 +58,19 @@ def read_serial():
 
     if data:
         # If data was received, decode it as JSON and update the labels with the received data
-        if cap and freq:
-            capmem_label.config(text=cap)
-            freqmem_label.config(text=freq)
+        capmem1_label.config(text=capmem_label.cget("text"))
+        freqmem1_label.config(text=freqmem_label.cget("text"))
+
+        capmem_label.config(text=capacitance_label.cget("text"))
+        freqmem_label.config(text=frequency_label.cget("text"))
         
         json_data = json.loads(data)
         cap = json_data['cap']
         freq = json_data['freq']
+        unit = json_data['unit']
         capacitance_label.config(text=cap)
         frequency_label.config(text=freq)
+        capacitance_heading.config(text="Capacitance [" + unit + "]")
 
     # Schedule the function to run again after 100ms
     root.after(100, read_serial)
