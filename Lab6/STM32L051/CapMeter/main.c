c0d66003ea02dc1604c99f1ae8b5a781cc08a2ec
Marcus <75653877+Beluguy@users.noreply.github.com>
2023-03-14 14:55:50 -0700

    working

--- Lab6/STM32L051/CapMeter/main.c
+++ Lab6/STM32L051/CapMeter/main.c
@@ -10,10 +10,10 @@
                              LCD_RS PA0 -|6       27|- PB4
                              LCD_E  PA1 -|7       26|- PB3
                              LCD_D4 PA2 -|8       25|- PA15
-                             LCD_D5 PA3 -|9       24|- PA14 (update pushbutton)
+                             LCD_D5 PA3 -|9       24|- PA14 
                              LCD_D6 PA4 -|10      23|- PA13
                              LCD_D7 PA5 -|11      22|- PA12
-                                    PA6 -|12      21|- PA11 (memory pushbutton)
+                                    PA6 -|12      21|- PA11 (update pushbutton)
    (Measure the period at this pin) PA7 -|13      20|- PA10 (Reserved for RXD)
                                     PB0 -|14      19|- PA9  (Reserved for TXD)
                                     PB1 -|15      18|- PA8  (unit changing pushbutton)
@@ -64,11 +64,6 @@ void Configure_Pins (void)
 	// Activate pull up for pin PA11:
 	GPIOA->PUPDR |= BIT22; 
 	GPIOA->PUPDR &= ~(BIT23); 
-
-    GPIOA->MODER &= ~(BIT28 | BIT29); // Make pin PA14 update pushbutton input
-	// Activate pull up for pin PA14:
-	GPIOA->PUPDR |= BIT28; 
-	GPIOA->PUPDR &= ~(BIT29); 
     //---------------------------------------------------------
 
     //-----------------------for 555timer----------------------
@@ -83,7 +78,6 @@ void main(void)
 {  
     Configure_Pins();
 	LCD_4BIT();
-    int current11, current8, previous11, previous8;
 
     //----------------from period--------------
     long int count;
@@ -100,22 +94,15 @@ void main(void)
 	
 	//WARNING: notice that printf() of floating point numbers is not enabled in the makefile!
     waitms(500);
-    printf("STM32L051 Capacitance measurement at pin PA7.\r\n");
+    //printf("STM32L051 Capacitance measurement at pin PA7.\r\n");
 	LCDprint("C measured [nF]:\r\n", 1, 1);
-    previous8=(GPIOA->IDR&BIT8)?0:1;       //read PA8, should be 1
-    previous11=(GPIOA->IDR&BIT11)?0:1;      //read PA11, should be 1
     while(1)
 	{   
-		count=GetPeriod(100);
-        current11=(GPIOA->IDR&BIT11)?1:0;  	//read PA11
-        current8=(GPIOA->IDR&BIT8)?1:0;     //read PA8
-
-		while(current11==previous11)     	// update
-		{
-            if(current8!=previous8)       	//unit
-			{
-			previous8=current8;
-            units = !units;
+		while(GPIOA->IDR&BIT11)     		// while update isn't pressed
+		{	
+            if(!(GPIOA->IDR&BIT8))       	//if unit is pressed
+			{	
+				units =! units;
                 if (units == 0)
                 {
                     LCDprint("C measured [nF]:", 1, 1);
@@ -134,37 +121,30 @@ void main(void)
                     sprintf(buff, "%.3f %.3f", capacitance, cap_old);
                     LCDprint(buff, 2, 1);
                 }
+				waitms(200);
 			}
+			waitms(50);
 		}
-		if(current11!=previous11)       	
-		{
-			previous11=current11;
-		}
-
-        cap_old = capacitance;
+		waitms(50);
+		
+		count=GetPeriod(100);
+		T = count / (F_CPU*100.0); // Since we have the time of 100 periods, we need to divide by 100
+		f = 1.0 / T;
+		cap_old = capacitance;
         capacitance = conversion_factor * T / (0.693 * RESISTANCE * 3.0);
-    	
-		if(count>0)						// print to putty
-		{
-			T = count / (F_CPU*100.0); // Since we have the time of 100 periods, we need to divide by 100
-			f = 1.0 / T;
-			printf("f=%.2fHz, C=%.1f nF\r", f, capacitance);
-		}
-		else
-		{
-			printf("NO SIGNAL\r");
-		}
 
-        if (units == 0)					// print to LCD
+        if (units == 0)
         {
             sprintf(buff, "%.1f %.1f", capacitance, cap_old);
+			printf("{\"cap\":%.2f,\"freq\":%.2f,\"unit\":\"nF\"}\r\n", capacitance, f);
         }
         else
         {
             sprintf(buff, "%.3f %.3f", capacitance, cap_old);
+			printf("{\"cap\":%.2f,\"freq\":%.2f,\"unit\":\"uF\"}\r\n", capacitance, f);
         }
         LCDprint(buff, 2, 1);
         fflush(stdout); // GCC printf wants a \n in order to send something.  If \n is not present, we fflush(stdout)
-		waitms(200);
+		waitms(50);
 	}
 }
