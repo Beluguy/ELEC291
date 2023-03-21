d4f20d3c6a05fadf794833c40bf3d87f2e1a98dc
Joe <skipfie@gmail.com>
2023-03-21 15:42:17 -0700

    add buzzer and integrate command parser

--- Magnetic-Field-Controlled-Robot/RX-STM32/Robot_Base/main.c
+++ Magnetic-Field-Controlled-Robot/RX-STM32/Robot_Base/main.c
@@ -5,6 +5,68 @@
 #include "adc.h"
 #include "macros.h"
 #include "../LCD/lcd.h"
+#include "pitches.h"
+
+#define SPKR_F 520L
+
+int melody[] = {
+
+  //Based on the arrangement at https://www.flutetunes.com/tunes.php?id=192
+  
+  NOTE_E6, 4,  NOTE_B5,8,  NOTE_C6,8,  NOTE_D6,4,  NOTE_C6,8,  NOTE_B5,8,
+  NOTE_A5, 4,  NOTE_A5,8,  NOTE_C6,8,  NOTE_E6,4,  NOTE_D6,8,  NOTE_C6,8,
+  NOTE_B5, -4,  NOTE_C6,8,  NOTE_D6,4,  NOTE_E6,4,
+  NOTE_C6, 4,  NOTE_A5,4,  NOTE_A5,8,  NOTE_A5,4,  NOTE_B5,8,  NOTE_C6,8,
+
+  NOTE_D6, -4,  NOTE_F5,8,  NOTE_A6,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E6, -4,  NOTE_C6,8,  NOTE_E6,4,  NOTE_D6,8,  NOTE_C6,8,
+  NOTE_B5, 4,  NOTE_B5,8,  NOTE_C6,8,  NOTE_D6,4,  NOTE_E6,4,
+  NOTE_C6, 4,  NOTE_A5,4,  NOTE_A5,4, REST, 4,
+
+  NOTE_E6, 4,  NOTE_B5,8,  NOTE_C6,8,  NOTE_D6,4,  NOTE_C6,8,  NOTE_B5,8,
+  NOTE_A5, 4,  NOTE_A5,8,  NOTE_C6,8,  NOTE_E6,4,  NOTE_D6,8,  NOTE_C6,8,
+  NOTE_B5, -4,  NOTE_C6,8,  NOTE_D6,4,  NOTE_E6,4,
+  NOTE_C6, 4,  NOTE_A5,4,  NOTE_A5,8,  NOTE_A5,4,  NOTE_B5,8,  NOTE_C6,8,
+
+  NOTE_D6, -4,  NOTE_F5,8,  NOTE_A6,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E6, -4,  NOTE_C6,8,  NOTE_E6,4,  NOTE_D6,8,  NOTE_C6,8,
+  NOTE_B5, 4,  NOTE_B5,8,  NOTE_C6,8,  NOTE_D6,4,  NOTE_E6,4,
+  NOTE_C6, 4,  NOTE_A5,4,  NOTE_A5,4, REST, 4,
+  
+
+  NOTE_E6,2,  NOTE_C6,2,
+  NOTE_D6,2,   NOTE_B5,2,
+  NOTE_C6,2,   NOTE_A5,2,
+  NOTE_GS5,2,  NOTE_B5,4,  REST,8, 
+  NOTE_E6,2,   NOTE_C6,2,
+  NOTE_D6,2,   NOTE_B5,2,
+  NOTE_C6,4,   NOTE_E6,4,  NOTE_A6,2,
+  NOTE_GS6,2,
+
+};
+
+// change this to make the song slower or faster
+#define TEMPO 300
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+int wholenote = (60000 * 4) / TEMPO;
+
+int divider = 0, noteDuration = 0;
+
+// Some 'defines' to turn pins on/off easily (pins must be configured as outputs)
+#define PB3_0 (GPIOB->ODR &= ~BIT3)
+#define PB3_1 (GPIOB->ODR |=  BIT3)
+#define PB4_0 (GPIOB->ODR &= ~BIT4)
+#define PB4_1 (GPIOB->ODR |=  BIT4)
+#define PB5_0 (GPIOB->ODR &= ~BIT5)
+#define PB5_1 (GPIOB->ODR |=  BIT5)
+#define PB6_0 (GPIOB->ODR &= ~BIT6)
+#define PB6_1 (GPIOB->ODR |=  BIT6)
+#define PB7_0 (GPIOB->ODR &= ~BIT7)
+#define PB7_1 (GPIOB->ODR |=  BIT7)
 
 // LQFP32 pinout
 //                 ----------
@@ -27,7 +89,6 @@
 //                 ----------
 
 //  ----------------------------------------- GLOBAL VARS ------------------------------------------------------------
-unsigned char mode=0;
 
 // Interrupt service routines are the same as normal
 // subroutines (or C funtions) in Cortex-M microcontrollers.
@@ -49,17 +110,71 @@ void TIM21_Handler(void)
                 case 0:
                     break;
                 case 1:
-                    mode = 0;
-                    LCDprint("Automatic", 1, 1);
+                    printf("togglemode\n");
+                    togglemode();
                     break;
                 case 2:
-                    mode = 1;
-                    LCDprint("Manual", 1, 1);
+                    // fwd
+                    printf("fwd\n");
+                    PB6_0;
+                    PB5_1;
+                    PB4_0;
+                    PB3_1;
                     break;
                 case 3:
-
+                    // back
+                    printf("back\n");
+                    PB6_1;
+                    PB5_0;
+                    PB6_0;
+                    PB5_1;
                     break;
                 case 4:
+                    // right
+                    printf("right\n");
+                    PB6_0;
+                    PB5_1;
+                    PB4_1;
+                    PB3_0;
+                    break;
+                case 5:
+                    // left
+                    printf("left\n");
+                    PB6_1;
+                    PB5_0;
+                    PB4_0;
+                    PB3_1;
+                    break;
+                case 6:
+                    //tetris
+                    printf("tetris\n");
+                    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2)
+                    {
+
+                        // calculates the duration of each note
+                        divider = melody[thisNote + 1];
+                        if (divider > 0)
+                        {
+                            // regular note, just proceed
+                            noteDuration = (wholenote) / divider;
+                        }
+                        else if (divider < 0)
+                        {
+                            // dotted notes are represented with negative durations!!
+                            noteDuration = (wholenote) / abs(divider);
+                            noteDuration *= 1.5; // increases the duration in half for dotted notes
+                        }
+
+                        // we only play the note for 90% of the duration, leaving 10% as a pause
+                        tone(melody[thisNote], noteDuration * 0.9);
+
+                        // Wait for the specief duration before playing the next note.
+                        waitms(noteDuration);
+                    }
+                    break;
+                case 7:
+                    printf("following dist\n");
+                    //following dist
                     break;
                 default:
                     mode = 0;
@@ -73,6 +188,8 @@ void TIM21_Handler(void)
 void Hardware_Init(void)
 {
 	RCC->IOPENR  |= (BIT1|BIT0);         // peripheral clock enable for ports A and B
+
+    GPIOA->OSPEEDR=0xffffffff; // All pins of port A configured for very high speed! Page 201 of RM0451
 
 	// Configure the pin used for analog input: PB0 and PB1 (pins 14 and 15)
 	GPIOB->MODER |= (BIT0|BIT1);  // Select analog mode for PB0 (pin 14 of LQFP32 package)
@@ -130,14 +247,38 @@ void Hardware_Init(void)
     GPIOA->MODER = (GPIOA->MODER & ~(BIT24|BIT25)) | BIT24; // Make pin PA12 output (page 200 of RM0451, two bits used to configure: bit0=1, bit1=0)
 	GPIOA->OTYPER &= ~BIT12; // Push-pull
 
+	// Configure PA15 for altenate function (TIM2_CH1, pin 25 in LQFP32 package)
+	GPIOA->OSPEEDR  |= BIT30; // MEDIUM SPEED
+	GPIOA->OTYPER   &= ~BIT15; // Push-pull
+	GPIOA->MODER    = (GPIOA->MODER & ~(BIT30)) | BIT31; // AF-Mode
+	GPIOA->AFR[1]   |= BIT30 | BIT28 ; // AF5 selected (check table 16 in page 43 of "en.DM00108219.pdf")
+	
 	// Set up timer 2
+	RCC->APB1ENR |= BIT0;  // turn on clock for timer2 (UM: page 177)
+	TIM2->ARR = SYSCLK/SPKR_F-1;
+	TIM2->CR1 |= BIT4;      // Downcounting    
+	TIM2->CR1 |= BIT7;      // ARPE enable    
+	TIM2->DIER |= BIT0;     // enable update event (reload event) interrupt 
+	TIM2->CR1 |= BIT0;      // enable counting    
+	
+	// Enable PWM in channel 1 of Timer 2
+	TIM2->CCMR1|=BIT6|BIT5; // PWM mode 1 ([6..4]=110)
+	TIM2->CCMR1|=BIT3; // OC1PE=1
+	TIM2->CCER|=BIT0; // Bit 0 CC1E: Capture/Compare 1 output enable.
+	
+	// Set PWM to 50%
+	TIM2->CCR1=(SYSCLK/(SPKR_F*2));
+	TIM2->EGR |= BIT0; // UG=1
+    TIM2->CR1 &= ~BIT0; // disable timer
+
+/* 	// Set up timer 2
 	RCC->APB1ENR |= BIT0;  // turn on clock for timer2 (UM: page 177)
 	TIM2->ARR = F_CPU/DEF_F-1;
 	NVIC->ISER[0] |= BIT15; // enable timer 2 interrupts in the NVIC
 	TIM2->CR1 |= BIT4;      // Downcounting    
 	TIM2->CR1 |= BIT7;      // ARPE enable    
 	TIM2->DIER |= BIT0;     // enable update event (reload event) interrupt 
-	TIM2->CR1 |= BIT0;      // enable counting    
+	TIM2->CR1 |= BIT0;      // enable counting  */   
 	
 	// Set up timer 21
 	RCC->APB2ENR |= BIT2;  // turn on clock for timer21 (UM: page 188)
@@ -152,18 +293,6 @@ void Hardware_Init(void)
 	__enable_irq();
 }
 
-// Some 'defines' to turn pins on/off easily (pins must be configured as outputs)
-#define PB3_0 (GPIOB->ODR &= ~BIT3)
-#define PB3_1 (GPIOB->ODR |=  BIT3)
-#define PB4_0 (GPIOB->ODR &= ~BIT4)
-#define PB4_1 (GPIOB->ODR |=  BIT4)
-#define PB5_0 (GPIOB->ODR &= ~BIT5)
-#define PB5_1 (GPIOB->ODR |=  BIT5)
-#define PB6_0 (GPIOB->ODR &= ~BIT6)
-#define PB6_1 (GPIOB->ODR |=  BIT6)
-#define PB7_0 (GPIOB->ODR &= ~BIT7)
-#define PB7_1 (GPIOB->ODR |=  BIT7)
-
 // A define to easily read PA14 (PA14 must be configured as input first)
 #define PA14 (GPIOA->IDR & BIT14)
 
@@ -174,6 +303,7 @@ int main(void)
     int j, v;
 	long int count, f;
     int L, R;
+    int newF, reload;
 
 	Hardware_Init(); // configure pins, adc, lcd
 	
@@ -209,6 +339,7 @@ int main(void)
     	egets_echo(buf, 31); // wait here until data is received
         R = atoi(buf);
 */
+
         if (mode == 0) {
             if (readADC(ADC_CHSELR_CHSEL8) > 100) { // read only if its on
                 L = readADC(ADC_CHSELR_CHSEL8);
@@ -237,6 +368,16 @@ int main(void)
                 PB3_0;
         }
 
+
+
+        if (PA14)
+        {
+        }
+        else
+        {
+           togglemode(); 
+        }
+
         /*
         j=readADC(ADC_CHSELR_CHSEL8);
 		v=(j*33000)/0xfff;
@@ -258,18 +399,6 @@ int main(void)
 		PrintNumber(v%10000, 10, 4);
 		eputs("V ");
 		*/
-    
-        /*
-		eputs("PA14=");
-		if(PA14)
-		{
-			eputs("1 ");
-		}
-		else
-		{
-			eputs("0 ");
-		}
-        */
 
         /*
 		// Not very good for high frequencies because of all the interrupts in the background
